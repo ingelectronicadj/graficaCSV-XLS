143ec5869145e0bf5394dd657b9bb35e03acbf3a
Diego Javier Mena Amado <glud@localhost.localdomain>
2016-09-30 12:37:57 -0500

    Se generan las curvas restantes para la representacion de las otras columnas del fichero-recurso .tsv

--- csvToPlot.py
+++ csvToPlot.py
@@ -3,8 +3,11 @@ import csv
 import datetime as dt
 import matplotlib.dates as mdates
 
+t = []
 x = []
 y = []
+z = []
+w = []
 
 with open('data.tsv','r') as csvfile:
     rows = csv.DictReader(csvfile, delimiter="\t")
@@ -13,13 +16,20 @@ with open('data.tsv','r') as csvfile:
         #tiempo = row['Fecha'].replace("/","")
         tiempo = row['Fecha']
         tiempo = dt.datetime.strptime(tiempo,'%d/%m/%Y').date()
-        w = row['OBSERVADO'].replace(",",".")
-        w = float(w)
-        #x = row['URRA'].replace(",",".")
-        #y = row['ARMA'].replace(",",".")
-        #z = row['CLAO'].replace(",",".")
-        x.append(tiempo)
-        y.append(w)
+        medido = row['OBSERVADO'].replace(",",".")
+        medido = float(medido)
+        metodo1 = row['URRA'].replace(",",".")
+        metodo1 = float(metodo1)
+        metodo2 = row['ARMA'].replace(",",".")
+        metodo2 = float(metodo2)
+        metodo3 = row['CLAO'].replace(",",".")
+        metodo3 = float(metodo3)
+        t.append(tiempo)
+        x.append(medido)
+        y.append(metodo1)
+        z.append(metodo2)
+        w.append(metodo3)
+
 #print(x,y)
 #exit()
 #dates = ['01/02/1991','01/03/1991','01/04/1991']
@@ -31,7 +41,10 @@ plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
 #plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 plt.gca().xaxis.set_major_locator(mdates.YearLocator())
 
-plt.plot(x,y, label='Parametro OBSERVADO')
+plt.plot(t,x, label='Parametro OBSERVADO')
+plt.plot(t,y, label='Parametro URRA')
+plt.plot(t,z, label='Parametro ARMA')
+plt.plot(t,w, label='Parametro CLAO')
 plt.xlabel('Rango temporal')
 plt.ylabel('Magnitud')
 plt.title('Representacion grafica de los datos\nCargando archivo .tsv')
